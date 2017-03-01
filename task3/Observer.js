eab1e9dfbafd47c8661ddd5c376783d3b09afc1c
唐道远 <tangdy0351@cairenhui.com>
2017-03-01 17:09:05 +0800

    add task3

--- task3/Observer.js
+++ task3/Observer.js
@@ -1,7 +1,7 @@
 function Observer(obj) {
-  this.data = obj;
-  this.content = {};
-  this.transformAll(obj);
+  this.data = obj; // 对象挂载点
+  this.$p = Array.prototype.slice.call(arguments,1)[0] || 'data'; // 实现事件冒泡储存父级 名字
+  this.transformAll(obj); // 对obj对象进行 遍历， 然后调用convat 进行defineProperty改写
 }
 
 Observer.prototype.transformAll = function(obj) {
@@ -10,13 +10,16 @@ Observer.prototype.transformAll = function(obj) {
     var key = keyarr[i];
     var value = obj[keyarr[i]];
     if (value instanceof Object ) {
-        new Observer(value); // value此时为对象，new Observer()会修改此对象，修改的结果展示在了obj当中。
+        new Observer(value,key); // value此时为对象，new Observer()会修改此对象，修改的结果展示在了obj当中。
     } else {
-      this.convat(key, value);
+      this.convat(key, value, this.$p); // $p  传入父key
     }
   }
 }
-Observer.prototype.convat = function(key, val) {
+
+Observer.prototype.content = {}
+
+Observer.prototype.convat = function(key, val, $p) {
   var self = this;
   Object.defineProperty(self.data, key, {
     get: function(){
@@ -25,9 +28,9 @@ Observer.prototype.convat = function(key, val) {
     },
     set: function(newval){
       console.log('你设置了 '+ key + ', ' + '新的值为 ' + newval);
-      self.emit(key, newval);
+      self.emit($p+ '.' + key, newval);  // 触发形式为 father.child    newval为传入信息
       if (newval instanceof Object ) {
-        new Observer(newval);
+        new Observer(newval, key);  // 如果改写为对象
       }
       val =  newval
     },
@@ -36,26 +39,31 @@ Observer.prototype.convat = function(key, val) {
   });
 }
 Observer.prototype.$watch = function(name, fn) {
-  var self = this;
   if (!this.content[name]) {
     this.content[name] = [];
   }
   this.content[name].push(fn);
-  // 将emit插入set方法
   return this;
 }
 Observer.prototype.emit = function(name) {
+  if (name.indexOf('.') !== -1) {
+    var parent = name.split('.')[0];
+    this.emit(parent);
+  }
+
   var info = Array.prototype.slice.call(arguments,1);
-  if (this.content[name]) {
+
+  if (this.content[name]) {   // 遍历同名事件仓库 执行
     for (var i =0,len=this.content[name].length;i<len;i++) {
       var fn = this.content[name][i];
       fn.apply(this, info);
     }
   }
+
   return this;
 }
 
-Observer.prototype.off = function(name, fn) {
+Observer.prototype.off = function(name, fn) {  // 解除绑定，如果不传入函数名则取消所有同名事件
   if (!fn) {
     this.content[name] = null;
     return this;
@@ -65,38 +73,57 @@ Observer.prototype.off = function(name, fn) {
   return this;
 }
 
+
+let app2 = new Observer({
+    name: {
+        firstName: 'shaofeng',
+        lastName: 'liang'
+    },
+    age: 25
+});
+
+app2.$watch('name', function (newName) {
+    console.log('我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。')
+});
+//
+app2.data.name.firstName = 'hahaha';
+// 输出：我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。
+app2.data.name.lastName = 'blablabla';
+// 输出：我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。
 // -------------------------------------------------
-function tangEvent() {
-    this.content = {}
-}
-tangEvent.prototype.on = function(name, fn) {
-  if (!this.content[name]) {
-    this.content[name] = [];
-  }
-  this.content[name].push(fn);
-  return this;
-}
-tangEvent.prototype.emit = function(name) {
-  var info = Array.prototype.slice.call(arguments,1);
-  if (this.content[name]) {
-    for (var i =0,len=this.content[name].length;i<len;i++) {
-      var fn = this.content[name][i];
-      fn.apply(this, info);
-    }
-  }
-  return this;
-}
-tangEvent.prototype.off = function(name, fn) {
-  if (!fn) {
-    this.content[name] = null;
-    return this;
-  }
-  var index = this.content[name].indexOf(fn);
-  this.content[name].splice(index, 1);
-  return this;
-}
 
-var te = new tangEvent();
-te.on('wahaha',function(info){console.log('你出发了娃哈哈',info)});
-te.on('wahaha',function(info){console.log('你出发了娃哈哈1',info)});
-te.emit('wahaha','这是我传去的信息');
+//
+// function tangEvent() {
+//     this.content = {}
+// }
+// tangEvent.prototype.on = function(name, fn) {
+//   if (!this.content[name]) {
+//     this.content[name] = [];
+//   }
+//   this.content[name].push(fn);
+//   return this;
+// }
+// tangEvent.prototype.emit = function(name) {
+//   var info = Array.prototype.slice.call(arguments,1);
+//   if (this.content[name]) {
+//     for (var i =0,len=this.content[name].length;i<len;i++) {
+//       var fn = this.content[name][i];
+//       fn.apply(this, info);
+//     }
+//   }
+//   return this;
+// }
+// tangEvent.prototype.off = function(name, fn) {
+//   if (!fn) {
+//     this.content[name] = null;
+//     return this;
+//   }
+//   var index = this.content[name].indexOf(fn);
+//   this.content[name].splice(index, 1);
+//   return this;
+// }
+//
+// var te = new tangEvent();
+// te.on('wahaha',function(info){console.log('你出发了娃哈哈',info)});
+// te.on('wahaha',function(info){console.log('你出发了娃哈哈1',info)});
+// te.emit('wahaha','这是我传去的信息');
